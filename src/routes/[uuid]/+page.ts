53c11efd22d85b1b65d002106024a2d0c61bd9b3
Xin-10 <xin-lis@outlook.com>
2025-05-27 09:50:41 -0500

    Update item page layout and fix load error

--- src/routes/[uuid]/+page.ts
+++ src/routes/[uuid]/+page.ts
@@ -1,6 +1,7 @@
+import type { PageLoad } from './$types';
 import { fetchItem } from '@digitalculture/ochre-sdk';
 
-export const load = async ({ params }) => {
+export const load: PageLoad = async ({ params }) => {
   const { error, item } = await fetchItem(params.uuid, 'spatialUnit');
   if (error) throw new Error('Failed to load item');
   return { item };
